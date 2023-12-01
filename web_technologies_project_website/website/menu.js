cce0dc15765e5c586c531b971830ca88206a28f4
AcerNitro_Khanh <fhs49808h-salzburg.ac.at>
2023-12-01 14:51:45 +0100

    Kommentierung von Code

--- web_technologies_project_website/website/menu.js
+++ web_technologies_project_website/website/menu.js
@@ -1,28 +1,29 @@
-const mainMenu = document.querySelector('.nav-list');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
-const menu_items = document.querySelectorAll('nav .nav-list li a');
+// Auswahl der DOM-Elemente:
+const mainMenu = document.querySelector('.nav-list'); // Wählt das Navigationsmenü mit der Klasse 'nav-list' aus.
+const closeMenu = document.querySelector('.closeMenu'); // Wählt das Element mit der Klasse 'closeMenu' aus.
+const openMenu = document.querySelector('.openMenu'); // Wählt das Element mit der Klasse 'openMenu' aus.
+const menu_items = document.querySelectorAll('nav .nav-list li a'); // Wählt alle Anker-Elemente innerhalb von Listenelementen im Navigationsmenü aus.
 
+// Hinzufügen von Event-Listenern:
+openMenu.addEventListener('click', show); // Horcht auf ein Klick-Ereignis auf dem openMenu-Element und ruft die 'show'-Funktion auf.
+closeMenu.addEventListener('click', close); // Horcht auf ein Klick-Ereignis auf dem closeMenu-Element und ruft die 'close'-Funktion auf.
 
-
-
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
-
-// close menu when you click on a menu item 
+// Menü schließen, wenn auf ein Menüelement geklickt wird
 menu_items.forEach(item => {
     item.addEventListener('click', function () {
-        close();
+        close(); // Horcht auf Klick-Ereignisse auf jedem Menüelement und ruft die 'close'-Funktion auf, wenn geklickt wird.
     })
 })
 
+// Funktionen zur Ereignisbehandlung:
 function show() {
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+    mainMenu.style.display = 'flex'; // Zeigt das Navigationsmenü, indem der Anzeigestil auf 'flex' gesetzt wird.
+    mainMenu.style.top = '0'; // Setzt die obere Position des Navigationsmenüs auf '0'.
 }
+
 function close() {
-    mainMenu.style.top = '-100vh';
+    mainMenu.style.top = '-100vh'; // Versteckt das Navigationsmenü, indem die obere Position auf '-100vh' (außerhalb des Bildschirms) gesetzt wird.
     setTimeout(function () {
-        mainMenu.style.display = 'none';
+        mainMenu.style.display = 'none'; // Nach einer Verzögerung von 1000 Millisekunden (1 Sekunde) wird die Anzeige des Navigationsmenüs auf 'none' gesetzt.
     }, 1000);
 }
--- web_technologies_project_website/website/scraperW.js
+++ web_technologies_project_website/website/scraperW.js
@@ -1,6 +1,8 @@
+// Die Funktion getData() wird aufgerufen, um Daten abzurufen und anzuzeigen.
 getData();
 
 function getData() {
+    // AJAX-Anfrage an scraper.php, um Daten von der angegebenen URL zu erhalten.
     $.ajax({
         url: 'scraper.php',
         type: 'POST',
@@ -10,9 +12,12 @@ function getData() {
         },
         dataType: 'html',
         success: function (res) {
+            // Arrays für alle Einträge und gefilterte Einträge erstellen.
             let entries_all = [];
             let entries1 = $(res).find(".tablehell");
             let entries2 = $(res).find(".tabledunkel");
+
+            // Daten aus den HTML-Elementen extrahieren und in entries_all speichern.
             for (let i = 0; i < entries1.length; i++) {
                 let data = $(entries1[i]).find("td");
                 entries_all.push({
@@ -23,6 +28,8 @@ function getData() {
                     "Halle": $(data[7]).html(),
                     "Karte": $(data[8]).html().split("&nbsp;")[1]
                 });
+
+                // Wenn noch Einträge in entries2 vorhanden sind, auch diese verarbeiten.
                 if (i < entries2.length) {
                     let data = $(entries2[i]).find("td");
                     entries_all.push({
@@ -36,6 +43,7 @@ function getData() {
                 }
             }
 
+            // Einträge filtern, um nur diejenigen mit "ATV Schladming" als Heim- oder Gastteam zu behalten.
             let entries_filtered = [];
             for (let i = 0; i < entries_all.length; i++) {
                 if (
@@ -45,6 +53,7 @@ function getData() {
                     entries_filtered.push(entries_all[i]);
             }
 
+            // Generiere eine HTML-Tabelle und fülle sie mit den gefilterten Daten.
             let html = "<table>";
             html +=
                 "<tr><th>Datum</th><th>Zeit</th><th>Heimteam</th><th>Gastteam</th><th>Halle</th><th>Karte</th></tr>";
@@ -65,6 +74,8 @@ function getData() {
                     "</td></tr>";
             }
             html += "</table>";
+
+            // Füge die generierte Tabelle in das HTML-Element mit der ID #tableDataDamen ein.
             $("#tableDataDamen").html(html);
         },
     });
